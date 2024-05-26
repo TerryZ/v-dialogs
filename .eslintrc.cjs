56c121d612442ccab935b45fba90b604a9b9c66f
TerryZ <terry5@foxmail.com>
2024-05-26 12:01:49 +0800

    chore: 修复 eslint 不起作用的问题

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,19 +3,15 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 const path = require('node:path')
 const createAliasSetting = require('@vue/eslint-config-standard/createAliasSetting')
-
+// '@vue/eslint-config-prettier/skip-formatting'
 module.exports = {
   root: true,
   extends: [
     'plugin:vue/vue3-strongly-recommended',
-    '@vue/eslint-config-standard',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-standard'
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    ecmaFeatures: {
-      jsx: true
-    }
+    ecmaVersion: 'latest'
   },
   settings: {
     ...createAliasSetting({
