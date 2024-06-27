f8f14bb816ae424544a913c7d9b9c368435191d2
TerryZ <terry5@foxmail.com>
2024-06-27 15:39:08 +0800

    feat: 移除不再使用的实例

--- examples/main.js
+++ examples/main.js
@@ -4,16 +4,7 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Dialogs from '@/'
-// import Page from '@'
 
-// const app = createApp(App)
-
-// app.use(Page, {
-//   align: 'left',
-//   language: 'en'
-// })
-
-// app.mount('#app')
 const app = createApp(App)
 app.use(router)
 app.use(Dialogs)
