f9ba3ced468f9144f64c917df82fdcb7600dd859
TerryZ <terry5@foxmail.com>
2024-07-02 12:02:00 +0800

    feat: Drawer dts 文档

---
+++ types/drawer.d.ts
@@ -0,0 +1,50 @@
+import { DialogBaseOption, ContainerDialog, ContainerDialogBox } from './base'
+
+declare interface DrawerOption extends DialogBaseOption {
+  /**
+   * Display the header
+   * @default true
+   */
+  header?: boolean
+  /**
+   * The title text displayed in header
+   */
+  title?: string
+  /**
+   * Dialog width
+   */
+  width?: number
+  /**
+   * Dialog height
+   */
+  height?: number
+  /** The parameters pass to component as props */
+  params?: Record<string, unknown>
+  /**
+   * The close dialog button in header
+   * @default true
+   */
+  closeButton?: boolean
+  /**
+   * The drawer dialog placement
+   * @default 'right'
+   */
+  placement?: 'left' | 'right' | 'top' | 'bottom'
+  /**
+   * Backdrop click to close dialog
+   * @default true
+   */
+  backdropClose?: boolean
+}
+
+/**
+ * Display a drawer dialog
+ * @param component - The component to display
+ * @param options - custom options
+ * @returns the method to close dialog
+ */
+export declare const DialogDrawer: typeof ContainerDialog<DrawerOption>
+/**
+ * Use component to display content in a drawer dialog
+ */
+export declare const DialogDrawerBox: ContainerDialogBox<DrawerOption>
