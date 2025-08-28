52775bc40323f1b0ca61aa376d97d5a7377e1b06
Terry <terry5@foxmail.com>
2025-08-28 23:36:50 +0800

    feat: 图标样式更新

--- src/icons/IconMaximize.vue
+++ src/icons/IconMaximize.vue
@@ -3,10 +3,10 @@
     xmlns="http://www.w3.org/2000/svg"
     width="16"
     height="16"
-    fill="currentColor"
-    class="v-dialog-icon bi-square"
     viewBox="0 0 16 16"
-  >
-    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
-  </svg>
+    class="v-dialog-icon"
+  ><path
+    fill="currentColor"
+    d="M4.5 3A1.5 1.5 0 0 0 3 4.5v7A1.5 1.5 0 0 0 4.5 13h7a1.5 1.5 0 0 0 1.5-1.5v-7A1.5 1.5 0 0 0 11.5 3zM5 4.5h6a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-.5.5H5a.5.5 0 0 1-.5-.5V5a.5.5 0 0 1 .5-.5"
+  /></svg>
 </template>
