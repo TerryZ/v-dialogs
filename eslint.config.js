4426156af1e4b717bb2a2684cc9e706e41343910
Terry <terry5@foxmail.com>
2025-08-29 00:37:58 +0800

    chore: eslint 忽略 types 目录里的 dts 内容

--- eslint.config.js
+++ eslint.config.js
@@ -3,18 +3,29 @@ import standard from '@vue/eslint-config-standard'
 import globals from 'globals'
 
 export default [
+  {
+    ignores: [
+      'types/**'
+    ]
+  },
   ...pluginVue.configs[
     'flat/strongly-recommended'
     // 'plugin:vue/strongly-recommended'
   ],
   ...standard,
   {
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       sourceType: 'module',
       ecmaVersion: 'latest',
       globals: {
         ...globals.browser,
         ...globals.node
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true // 启用 JSX 支持
+        }
       }
     }
   }
